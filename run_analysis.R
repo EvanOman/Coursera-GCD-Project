2cb165c037aa32aafea90acee2ead6b155f54aa6
EvanOman <evan058@gmail.com>
2015-01-22 21:29:56 -0600

    Minor edits

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,3 @@
-
-
-#My process:
-    #Import x_train, subject_train, y_train, x_test, subject_test, y_test
-    #Merge x_train and y_test
-    #Merge suject_train and subject_train
-    #Merge y_train and y_test
-    #Apply labels subset from features.txt
-    #Make subjects factors
-    #Make activities factors using activity_labels
-
-#Not including values 555-561 because they are calculated values that take mean values as a parameter rather than being means of measurements themselves.
-
 require("data.table")
 require("dplyr")
 
